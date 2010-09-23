2f04a976e75ed43bc53cc9e2ef5279d85e86d536
Ryan Florence <rpflorence@gmail.com>
2010-09-22 22:38:34 -0600

    Class is passing the original test. Now need to test each method individually. For some reason it's not cleaning up :\

--- lib/garuda.rb
+++ lib/garuda.rb
@@ -7,30 +7,33 @@ class Garuda
   
   def initialize
     # set variables
-    repository = ENV['repository'] = ARGV[0]
-    ref_type   = ENV['ref_type']   = ARGV[1]
-    ref_name   = ENV['ref_name']   = ARGV[2]
-    
-    # load the application config
-    app_config = YAML::load(File.open('config.yml'))
-
-    # load the repository config
-    config = YAML::load(File.open("#{app_config['config']}/#{repository}.yml"))
-
-    # clone the repository
-    tmp_dir = 'tmp/' + Time.now.strftime("%Y%m%d%H%M%S")
-    Dir.mkdir(tmp_dir);
-    Dir.chdir(tmp_dir);
-    `git clone ../../#{app_config['repos']}/#{repository}.git`
-    Dir.chdir(repository)
-    tree = ENV['tree'] = Dir.pwd
+    @repository = ENV['repository'] = ARGV[0]
+    @ref_type   = ENV['ref_type']   = ARGV[1]
+    @ref_name   = ENV['ref_name']   = ARGV[2]
+    @app_config = YAML::load(File.open('config.yml'))
+    @config     = YAML::load(File.open("#{@app_config['config']}/#{@repository}.yml"))
+  end
+  
+  def clone_repository
+    @tmp_dir = 'tmp/' + Time.now.strftime("%Y%m%d%H%M%S")
+    Dir.mkdir(@tmp_dir);
+    Dir.chdir(@tmp_dir);
+    `git clone ../../#{@app_config['repos']}/#{@repository}.git`
+    Dir.chdir(@repository)
+    @tree = ENV['tree'] = Dir.pwd
     Dir.chdir('../../../');
-
-    # run the scripts
-    Runner.new(config, ref_type, ref_name, 'bin/').run
-
+    self
+  end
+  
+  def run
+    Runner.new(@config, @ref_type, @ref_name, 'bin/').run
+    self
+  end
+  
+  def cleanup
     # clean up
-    FileUtils.rm_rf tmp_dir
+    FileUtils.rm_rf @tmp_dir
+    self
   end
   
 end
