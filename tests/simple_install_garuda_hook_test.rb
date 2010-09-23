04f90c7324c779ed5f6b1ffbaeac7a71dcc0572d
Ryan Florence <rpflorence@gmail.com>
2010-09-23 12:21:19 -0600

    Added some extra text, remove "coloring" code since it doesn't come across in the log when run remotely

--- tests/simple_install_garuda_hook_test.rb
+++ tests/simple_install_garuda_hook_test.rb
@@ -19,8 +19,10 @@ class SimpleInstallGarudaHookTest < Test::Unit::TestCase
     
     should "install the repo hook in test.git and make it executable" do
       Dir.chdir @local
+      puts "# Garuda test script: Cloning the 'server' Garuda to the 'local' Garuda."
       `git clone ../server/garuda`
       Dir.chdir 'garuda'
+      puts "# Garuda test script: Committing dummy file and pushing to initiate the hook on the 'server'"
       `touch foo; git add .; git commit -m 'nothing'; git push origin master`
       Dir.chdir @server
       assert File.exists?('test.git/hooks/post-receive')
