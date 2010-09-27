2b3ad2f380f8761ab772c958227e0aaca57599dd
Ryan Florence <rpflorence@gmail.com>
2010-09-27 13:18:23 -0600

    Small refactor, decreasing nested code, created a new method

--- lib/garuda.rb
+++ lib/garuda.rb
@@ -3,65 +3,63 @@ require 'yaml'
 require 'fileutils'
 
 class Garuda
-  
-  attr_accessor :tree
-  
-  def initialize
-    # set variables
-    @repository = ENV['repository'] = ARGV[0] || ENV['repository']
-    @ref_type   = ENV['ref_type']   = ARGV[1] || ENV['ref_type']
-    @ref_name   = ENV['ref_name']   = ARGV[2] || ENV['ref_name']
+
+  attr_accessor :tree, :config
+
+  def initialize()
+    # set from hook, or when `run` script is called manually with ARGV
+    @repository = ENV['repository'] || ARGV[0] 
+    @ref_type   = ENV['ref_type']   || ARGV[1] 
+    @ref_name   = ENV['ref_name']   || ARGV[2]
     @config     = YAML::load(File.open("config/#{@repository}.yml"))
+    @root       = Dir.pwd
   end
-  
+
   def clone_repository
     @tmp_dir = 'tmp/' + Time.now.strftime("%Y%m%d%H%M%S")
     Dir.mkdir 'tmp' unless File.directory?('tmp')
     Dir.mkdir(@tmp_dir);
     Dir.chdir(@tmp_dir);
-    # in /tmp/timestamp/
     `git clone ../../../#{@repository}.git`
     Dir.chdir(@repository)
     @tree = ENV['tree'] = Dir.pwd
-    # go back to /
-    Dir.chdir('../../..');
+    Dir.chdir(@root);
     self
   end
-  
+
   def run
+    # run the scripts from the tmp repository's directory tree root
     Dir.chdir(@tree)
     # Can match multiple ref_types, so we loop them
-    @config[@ref_type].each do |key, data|
-      # check if our ref_type matches the key in the config file
-      if @ref_name.match(/#{key}/)
-        # matched, run the scripts under that ref_name
-        @config[@ref_type][key].each do |script, args|
-          puts "# Running script: " + script
-          args.each do |k,v|
-            # check if key already exists in ENV
-            if ENV[k] != nil
-              puts "ENV['#{k}'] is already defined as #{ENV[k]}, please change the name of your key. Aborting."
-              Process.exit();
-            end
-            # assign environment variable
-            ENV[k] = v.to_s
-          end if args
-          # execute the script
-          system("./../../../bin/#{script}")
-          # clear ENV
-          args.each { |k,v| ENV[k] = nil } if args
-        end
-      end
-    end
-    
-    Dir.chdir('../../../')
+    @config[@ref_type].each{ |key, data| self.execute key if @ref_name.match /#{key}/ }
+    Dir.chdir(@root)
     self
   end
-  
+
+  def execute(key)
+    @config[@ref_type][key].each do |script, args|
+      puts "# Running script: " + script
+      self.set_args args if args
+      system("#@root/bin/#{script}")
+      # clear env vars
+      args.each { |k,v| ENV[k] = nil } if args
+    end
+  end
+
+  def set_args(args)
+    args.each do |k,v|
+      if ENV[k] != nil
+        puts "ENV['#{k}'] is already defined as #{ENV[k]}, please change the name of your key. Aborting."
+        abort
+      end
+      # assign it to env
+      ENV[k] = v.to_s
+    end
+  end
+
   def cleanup
-    # clean up
     FileUtils.rm_rf @tmp_dir
     self
   end
-  
+
 end
